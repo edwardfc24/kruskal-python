d6c7132f29ba0b2a972b89595053ac52cbad45f6
Leonardo Ruiz Decourt <ruizdecourt@gmail.com>
2021-03-12 21:31:40 -0400

    Se agrego comentario

--- kruskal.py
+++ kruskal.py
@@ -3,9 +3,5 @@ class Kruskal:
     def __init__(self):
         pass
 
-<<<<<<< HEAD
-=======
-#Repositorio clonado - David Banzer
->>>>>>> base-kruskal
-
-#Se pudo hacer el clone
+# Repositorio clonado - David Banzer
+# Repositorio Clonado - Leonardo Ruiz
